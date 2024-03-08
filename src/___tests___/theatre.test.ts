031639d2e49508860137b42ff26404c6165036a3
phyusin <rollyphyusinkhant7290@gmail.com>
2024-03-08 11:13:02 +0630

    wip for learning testing

---
+++ src/___tests___/theatre.test.ts
@@ -0,0 +1,9 @@
+describe('theatre', () => {
+    describe('get theatre route', () => {
+        describe('given the theatre does not exist', () => {
+            it('should return 404', () => {
+                expect(true).toBe(true)
+            })
+        })
+    })
+})
