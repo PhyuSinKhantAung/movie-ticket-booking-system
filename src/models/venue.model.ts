e89e8984f75ea19b3fce27a49d5d874ad0584052
Phyu Sin Khant Aung <rollyphyusinkhant7290@gmail.com>
2024-03-09 15:25:00 +0630

    fixed venue model structure

--- src/models/venue.model.ts
+++ src/models/venue.model.ts
@@ -4,24 +4,18 @@ export interface Venue extends Document {
   theatre: mongoose.Schema.Types.ObjectId;
   region: string;
   location: string;
-  seatRows: number;
-  seatColumns: number;
 }
 
 const VenueSchema = new mongoose.Schema<Venue>({
   theatre: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Theatre",
-    default: null,
+    required: true,
   },
 
   region: String,
 
   location: String,
-
-  seatRows: Number,
-
-  seatColumns: Number,
 });
 
 export default mongoose.model<Venue>("Venue", VenueSchema);
